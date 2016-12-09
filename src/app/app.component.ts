10f7e385f26871f207bfd9163ea3362ee105633a
Jason Cardinal <jason.brogrammer@gmail.com>
2016-12-09 17:16:56 -0600

    Better radius interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Garçon!!!!!!';
+  radius = 100;
+  x = null;
+  y = null;
+  onRadiusSliderChange(val) {
+    this.radius = val;
+  }
+  onDrag(event, data: any) {
+    console.log('MOUSE X: ', event.clientX);
+    console.log('MOUSE Y: ', event.clientY);
+    if(event.clientX > 0) { this.x = event.clientX; }
+    if(event.clientY > 0) { this.y = event.clientY; }
+    // event.preventDefault();
+  }
 }
